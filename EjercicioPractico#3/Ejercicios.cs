8e8d728edbad00c2152168f64d1d3c4d4fd12d45
Androxito <francogamez23@gmail.com>
2023-10-11 18:58:14 -0700

    Agregar archivos de proyecto.

---
+++ EjercicioPractico#3/Ejercicios.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioPractico_3
+{
+    internal class Ejercicios
+    {
+        public void Problema1()
+        {
+            int[,] arreglo = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+
+            for(int i = 0; i < arreglo.GetLength(0); i++)
+            {
+                for(int a = 0;a < arreglo.GetLength(1); a++)
+                {
+                    Console.Write(" "+arreglo[i,a]);
+                }
+                Console.WriteLine();
+            }
+        }
+        public void problema2()
+        {
+            string[,] Color = { {"Azul","Rojo" }, {"Verde","Amarrillo" } };
+            for(int i =0; i < Color.GetLength(0); i++)
+            {
+                for(int a = 0;a < Color.GetLength(1); a++)
+                {
+                    Console.Write(" "+ Color[i,a]);
+                }
+                Console.WriteLine();
+            }
+        }
+        public void problema3()
+        {
+            double[,] Suma = { {1.1,1.2,1.3 }, {1.4,1.5,1.6 }, {1.7,1.8,1.9 }, {2.0,2.1,2.2 } };
+            double respuesta = 0;
+
+            for(int i = 0;i < 4; i++)
+            {
+                for(int a = 0; a <3; a++)
+                {
+                    respuesta += Suma[i,a];
+                }
+            }
+            Console.WriteLine(respuesta);
+
+        }
+
+
+    }
+}
---
+++ EjercicioPractico#3/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace EjercicioPractico_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          Ejercicios ejercicios = new Ejercicios();
+            Console.WriteLine("Problema1");
+            ejercicios.Problema1();
+            Console.WriteLine();
+            Console.WriteLine("Problema2");
+            ejercicios.problema2();
+            Console.WriteLine();
+            Console.WriteLine("Problema3");
+            ejercicios.problema3();
+        }
+    }
+}
